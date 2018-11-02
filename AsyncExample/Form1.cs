398a8fb482cfb89bb4ae77961755a9d79bdba20a
AndySmithSL <42347047+AndySmithScott@users.noreply.github.com>
2018-11-02 15:43:55 +0000

    hoxfix2 commit 1

--- AsyncExample/Form1.cs
+++ AsyncExample/Form1.cs
@@ -135,7 +135,7 @@ namespace AsyncExample
         private void MyMethodIssue53()
         {
             textBoxResults.Text = "Issue 53";
-            textBoxResults.Text += "More Issue 53";
+            textBoxResults.Text += "More Issue 53 and issue 54";
         }
 
         //Another issue 53 change
